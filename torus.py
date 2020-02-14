d56a68629d9c4ffae2a587a3f558207b07a547e2
alexandre.faria95@gmail.com <alexandre.faria95@gmail.com>
2020-02-13 21:45:23 -0300

    Fix in schedule generator

--- torus.py
+++ torus.py
@@ -4,13 +4,52 @@ import time
 from random import randint
 
 #function to create a matrix of arbitrary size
-def createSchedule(lines, columns, upColumn):
+def createSchedule(lines, columns, upLine, upColumn):
+	# for line in range(lines):
+	# 	for column in range(columns):
+	# 		if(line == upLine and column <= (columns//2)+1):
+
 	if(lines <= 0 or columns <= 0 or upColumn > columns): 
 		return 0
 	else:
-		return [1 if (lin == upLine or x == upColumn) else 0 for col in range(columns) for y in range(lines)]
-		#now we fill the rows
+		matrix = [[1 if ( (lin == upLine and col < (columns//2)+1) or col == upColumn) else 0 for col in range(columns)] for lin in range(lines)]
+		schedule = []
+		for line in matrix:
+			schedule += line
+		return schedule
 
-a = (createSchedule(4,4,2))
-for line in a:
-	print(line)
+def main():
+
+	slotSize = 5000; #size of the schedule slot in ms
+	lines = columns = 4
+
+	schedule = createSchedule(lines, columns, 2, 2)
+	print(schedule)
+	return
+
+	scheduleIndex = 0
+
+	print("START", time.strftime("%Y/%m/%d, %H:%M:%S")) #start of the schedule
+	while True:
+
+		if(schedule[scheduleIndex]):
+			#interface on
+			print("Interface  ON", time.strftime("%H:%M:%S"))
+			os.system("ifconfig wlp3s0 up")
+		else:
+			#interfaceoff
+			print("Interface OFF", time.strftime("%H:%M:%S"))
+			os.system("ifconfig wlp3s0 down")
+
+
+
+		#while not at the end of the schedule indexes we increment the couter, else we refresh i
+		scheduleIndex = 0 if scheduleIndex >= len(schedule)-1 else scheduleIndex+1
+
+		#we now wait for the slottime to end
+		time.sleep(slotSize/1000)
+
+	return 0
+
+
+main()
