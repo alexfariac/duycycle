1aee0240c918968aa104a91e59879b6b86b93696
Alexandre <alexandre.faria95@gmail.com>
2020-04-04 21:09:55 -0300

    Alteracoes de automatizacao dos testes

--- client.py
+++ client.py
@@ -1,25 +1,44 @@
+import select
 import socket
+import sys
 import time
 import os
 
 UDP_IP = "192.168.1.2"
 UDP_PORT = 5000
 
+sock_addr = (UDP_IP, UDP_PORT)
 sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM,
-                     socket.IPPROTO_UDP) # UDP
+                     socket.SOCK_DGRAM, # UDP
+                     socket.IPPROTO_UDP) 
+
+#set the socket to nonblockable
+sock.setblocking(0)
 
 
 i = 0
-for x in range(100):
+reps = 1000
+slot_size = 1000
+
+for n in range(reps): #repeat the test multiple times
+
 	try:
-		time.sleep(1)
+		time.sleep((int)slot_size/1000) #wait for the slot time #we only try to send once each slot
 
+		sock.sendto(str(i).encode(), (UDP_IP, UDP_PORT))
+		
 
-		sock.sendto(str(i), (UDP_IP, UDP_PORT))
-		print(str(i))
+		#Espera retorno do receptor (foi esse o slot em que houve comunicacao ?)
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+		if data:
+			#recebemos retorno do receptor com o valor do primeiro slot de comunicacao
+			#resetar os parametros para um novo teste
+			# i = 0
+			print("RECEIVED", data)
+
 
 	except Exception as e:
+		#when conection is unreachable
 		print(e)
 		pass
 
--- server.py
+++ server.py
@@ -1,12 +1,26 @@
 import socket
+import time
 
 UDP_IP = "192.168.1.2"
 UDP_PORT = 5000
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+
+# sock.setblocking(0)
+
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+	try:
+	    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+	    if data:
+	    	print("received message:", data)
+	    	#enviar de volta a mensagem com o valor recebido
+	    	sock.sendto(str(data).encode(), ("192.168.1.1", 5000))
+
+	    else:
+	    	print("No data yet")
+
+	except Exception as e:
+		print(e)
