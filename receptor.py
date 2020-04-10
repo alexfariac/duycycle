acc98a69bafe0fc10972a9da6f5ba84a1eb8c625
Alexandre <alexandre.faria95@gmail.com>
2020-04-10 18:26:06 -0300

    Comentarios para receptor

--- receptor.py
+++ receptor.py
@@ -8,11 +8,12 @@ UDP_PORT = 5000
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
-sock.setblocking(0)
+sock.setblocking(0) #set the sock to do nonbloking read operations
 
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT)) #bind the socket to the port and addr 
 
-
+#this calls the duty cicle method using the parameters we passed
+#INTERFACE SLOT_SIZE DUTY_CICLE_METHOD DUTY_CICLE_PARAMS
 subprocess.Popen(['python3', 'schedule.py', 'wlp3s0', '1000', 'grid', '4', '4'])
 
 while True:
@@ -24,5 +25,6 @@ while True:
 	    	sock.sendto(str(data).encode(), addr)
 
 	except Exception as e:
-		#TODO TRATAR AS EXCESSOES ESPECIFICAMENTE #BlokingIOError: Errno 11
+		#In case of exceptions we just pass, many exceptions are raised by expecteded behavirous
+		#Such as  #BlokingIOError: Errno 11 and HostUnrecheables
 		pass
