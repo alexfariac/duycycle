716ca887af4b1fd7f655ad62699b2023a7bc30e9
Alexandre <alexandre.faria95@gmail.com>
2020-04-21 13:42:24 -0300

    Aumentando numero de repeticoes, enviando pacote de controle ao final das repeticoes e colocando endereco final de recepcao

--- enlaceQualityTest/sender.py
+++ enlaceQualityTest/sender.py
@@ -2,7 +2,7 @@ import socket
 import json
 
 
-UDP_IP = '127.0.0.1'
+UDP_IP = '192.168.1.2'
 UDP_PORT = 12000
 
 sock_addr = (UDP_IP, UDP_PORT)
@@ -10,15 +10,16 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM, # UDP
                      socket.IPPROTO_UDP) 
 
-SENDS = 100
+SENDS = 100000
 
 for rep in range(SENDS):
 	datagram = {'no': rep, 'total': SENDS, 'done':0}
 	datagram = json.dumps(datagram).encode()
 
+	print("Sending: {}".format(datagram))
 	sock.sendto(datagram, (UDP_IP, UDP_PORT)) #send the data
 
-while 1:
+for x in range(100):
 	datagram = {'done':1}
 	datagram = json.dumps(datagram).encode()
 	sock.sendto(datagram, (UDP_IP, UDP_PORT)) #send the data	
