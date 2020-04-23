4adcabe7ec863770968f20481c897f8e80c37d89
Alexandre <alexandre.faria95@gmail.com>
2020-04-23 19:44:37 -0300

    Melhorando o retorno do ack

--- enlaceQualityTest/receiver.py
+++ enlaceQualityTest/receiver.py
@@ -1,30 +1,34 @@
 import socket
 import json
 
-UDP_IP = "192.168.1.2"
+UDP_IP = "127.0.0.1"
 UDP_PORT = 12000
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
-sock.bind((UDP_IP, UDP_PORT))
-
+sock.bind((UDP_IP, UDP_PORT)) 
 
 revc = 0
 total = 1
 while 1:
 	data, addr = sock.recvfrom(1024)
 	data = json.loads(data.decode())
+	print(data)
 
 	if data['done']:
-		break
-
-	if data:
+		eq = (revc/total)*100
+		data = {"eq": eq, "revc": revc}
+		data = json.dumps(data).encode()
+		sock.sendto(data, addr)
+		
+		revc = 0
+		total = 1
+	else:
 		total = data['total']
 		revc+=1
 
-	print(data)
 
-eq = (revc/total)*100
-print("RECEIVED {} OUT OF {} PACKAGES".format(revc, total))
-print("LINK QUALITY : {}%".format(eq))
+# eq = (revc/total)*100
+# print("RECEIVED {} OUT OF {} PACKAGES".format(revc, total))
+# print("LINK QUALITY : {}%".format(eq))
--- enlaceQualityTest/sender.py
+++ enlaceQualityTest/sender.py
@@ -2,13 +2,15 @@ import socket
 import json
 
 
-UDP_IP = '192.168.1.2'
+UDP_IP = '127.0.0.1'
 UDP_PORT = 12000
 
 sock_addr = (UDP_IP, UDP_PORT)
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM, # UDP
                      socket.IPPROTO_UDP) 
+
+sock.setblocking(0)
 
 SENDS = 100000
 
@@ -19,7 +21,19 @@ for rep in range(SENDS):
 	print("Sending: {}".format(datagram))
 	sock.sendto(datagram, (UDP_IP, UDP_PORT)) #send the data
 
-for x in range(100):
-	datagram = {'done':1}
-	datagram = json.dumps(datagram).encode()
-	sock.sendto(datagram, (UDP_IP, UDP_PORT)) #send the data	
+while 1:
+	try:
+		datagram = {'done':1}
+		datagram = json.dumps(datagram).encode()
+		sock.sendto(datagram, (UDP_IP, UDP_PORT)) #send the data	
+
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+		data = json.loads(data.decode())
+
+		if data:
+			print("REVEIVED BACK")
+			print(data)
+			break;
+
+	except Exception as e:
+		pass
