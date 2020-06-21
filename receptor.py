eed9f8987e34caf38ded1f82807610f9d06172d9
Alexandre <alexandre.faria95@gmail.com>
2020-06-21 11:54:48 -0300

    Add P parameter of enlace quality

--- receptor.py
+++ receptor.py
@@ -2,6 +2,7 @@ import socket
 import time
 import subprocess
 import sys
+import random
 
 UDP_IP = "192.168.1.2"
 UDP_PORT = 5000
@@ -14,10 +15,10 @@ sock.setblocking(0) #set the sock to do nonbloking read operations
 sock.bind((UDP_IP, UDP_PORT)) #bind the socket to the port and addr 
 
 #python3 tranmissor.py {{reps}} {{interface}} {{slot_size}} {{duty_cicle_method}} {{duty_cicle_args}}
-args = sys.argv[1:6]
+args = sys.argv[1:7]
 
-INTERFACE, SLOT_SIZE, METHOD = args[0:3] 
-dutyCicleArgs = args[3:6]
+INTERFACE, SLOT_SIZE, P, METHOD = args[0:4] 
+dutyCicleArgs = args[4:7]
 
 args = ['python3','schedule.py', INTERFACE, SLOT_SIZE, METHOD]+dutyCicleArgs
 
@@ -25,13 +26,22 @@ args = ['python3','schedule.py', INTERFACE, SLOT_SIZE, METHOD]+dutyCicleArgs
 #INTERFACE SLOT_SIZE DUTY_CICLE_METHOD DUTY_CICLE_PARAMS
 subprocess.Popen(args)
 
+P = int(P)/100
 while True:
+
 	try:
 	    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 	    if data:
-	    	print("Received: ", data)
-	    	#enviar de volta a mensagem com o valor recebido, para o endereco e porta 
-	    	sock.sendto(data, addr)
+
+	    	r = random.random() #0.0 to 1.0
+	    	#P should be from 0 to 100 inclusive. 
+	    	recvPkg = r <= P #if the random number is smaller than or equal to the enlace quality operator, then the package would have benn received
+
+	    	print("Received: ", data, P, r)
+
+	    	if recvPkg:
+		    	#enviar de volta a mensagem com o valor recebido, para o endereco e porta 
+		    	sock.sendto(data, addr)
 
 	except Exception as e:
 		#In case of exceptions we just pass, many exceptions are raised by expecteded behavirous
